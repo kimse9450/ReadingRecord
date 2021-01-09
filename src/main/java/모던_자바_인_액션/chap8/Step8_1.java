d17448d759b1f0dcc751d578952058455a114249
geonhee.sung <jvckigh@gmail.com>
2021-01-10 03:36:56 +0900

    자바 인 액션 chap8.1 코드 추가

---
+++ src/main/java/모던_자바_인_액션/chap8/Step8_1.java
@@ -0,0 +1,13 @@
+package 모던_자바_인_액션.chap8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Step8_1 {
+    public static void main(String[] args) {
+        List<String> friends = new ArrayList<>();
+        friends.add("김세은");
+        friends.add("이예지");
+        friends.add("성건희");
+    }
+}
---
+++ src/test/java/모던_자바_인_액션/chap8/Step8_1Test.java
@@ -0,0 +1,57 @@
+package 모던_자바_인_액션.chap8;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Step8_1Test {
+    @Test
+    public void 단순_리스트_add() {
+        List<String> friends = new ArrayList<>();
+
+        friends.add("김세은");
+        friends.add("이예지");
+        friends.add("성건희");
+
+        assertThat(friends.get(0)).isEqualTo("김세은");
+        assertThat(friends.get(1)).isEqualTo("이예지");
+        assertThat(friends.get(2)).isEqualTo("성건희");
+        assertThat(friends.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void 팩토리_메서드_사용해서_add() {
+        List<String> friends = Arrays.asList("김세은", "이예지", "성건희");
+
+        assertThat(friends.get(0)).isEqualTo("김세은");
+        assertThat(friends.get(1)).isEqualTo("이예지");
+        assertThat(friends.get(2)).isEqualTo("성건희");
+        assertThat(friends.size()).isEqualTo(3);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void 고정_크기_리스트에_요소_추가시_UnsupportedOperationException_에러() {
+        List<String> friends = Arrays.asList("김세은", "이예지", "성건희");
+        friends.set(0, "레드"); // 갱신은 가능
+        friends.add("어둠의 레드"); // 추가 시 에러
+    }
+
+    @Test
+    public void 리스트를_HashSet으로_생성() {
+        Set<String> friends = new HashSet<>(Arrays.asList("김세은", "이예지", "성건희"));
+       
+        assertThat(friends.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void 리스트를_스트림API로_생성() {
+        Set<String> friends = Stream.of("김세은", "이예지", "성건희")
+                                    .collect(Collectors.toSet());
+
+        assertThat(friends.size()).isEqualTo(3);
+    }
+}
