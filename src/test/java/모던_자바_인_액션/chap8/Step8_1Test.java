ed3d42e15a19d22ccf2adbaa4b1a6af0866db294
geonhee.sung <jvckigh@gmail.com>
2021-01-13 02:16:47 +0900

    Update JUnit 5

--- src/test/java/모던_자바_인_액션/chap8/Step8_1Test.java
+++ src/test/java/모던_자바_인_액션/chap8/Step8_1Test.java
@@ -1,6 +1,7 @@
 package 모던_자바_인_액션.chap8;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -8,11 +9,11 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class Step8_1Test {
+class Step8_1Test {
+
     @Test
     public void 단순_리스트_add() {
         List<String> friends = new ArrayList<>();
-
         friends.add("김세은");
         friends.add("이예지");
         friends.add("성건희");
@@ -33,17 +34,18 @@ public class Step8_1Test {
         assertThat(friends.size()).isEqualTo(3);
     }
 
-    @Test(expected = UnsupportedOperationException.class)
+    @Test
     public void 고정_크기_리스트에_요소_추가시_UnsupportedOperationException_에러() {
         List<String> friends = Arrays.asList("김세은", "이예지", "성건희");
+
         friends.set(0, "레드"); // 갱신은 가능
-        friends.add("어둠의 레드"); // 추가 시 에러
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> friends.add("어둠의 레드"));
     }
 
     @Test
     public void 리스트를_HashSet으로_생성() {
         Set<String> friends = new HashSet<>(Arrays.asList("김세은", "이예지", "성건희"));
-       
+
         assertThat(friends.size()).isEqualTo(3);
     }
 
